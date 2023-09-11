d6ca0a15c8d52933d7bcc30aa57c4b71de0bb0ea
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2023-09-11 07:58:04 +0300

    finished nav and footer

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import logo from "../../assets/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./navbar.scss";
+import { useState } from "react";
+import Sidebar from "../Sidebar";
+
+const links = [
+  {
+    id: 1,
+    text: "الرئيسية",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "من نحن",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "الموزعون",
+    url: "/clients",
+  },
+];
+
+const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header ">
+          <Link to="/">
+            <img src={logo} alt="coffee store" />
+          </Link>
+          <button
+            type="button"
+            className="nav-toggle"
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          >
+            <FaBars />
+            <Sidebar isSidebarOpen={isSidebarOpen} />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+
+export { Navbar, Footer, Sidebar };
